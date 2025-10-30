239da61ac3ec1fb3347516db3aacf608997fe39a
Jane Ahone <jane.eloundou@gmail.com>
2025-10-30 04:16:58 +0100

    new

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,44 +1,9 @@
 import HeroSlider, { HeroSlide } from "@/components/component.heroslider";
+import FeatureComponent from "@/components/components.feature";
+import DividerLine from "@/components/molecules/DividerLine/page";
 import ThreeCardDisplay from "@/components/templates/ThreeCardDisplay/page";
 import React from "react";
 
-const heroSlides: HeroSlide[] = [
-  {
-    image:
-      "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=1920&q=80",
-    title: "Destination Highlights",
-    subtitle: "Explore Nigeria's iconic landmarks and hidden treasures",
-    alt: "Ocean sunset view",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5?w=1920&q=80",
-    title: "Luxury Experiences",
-    subtitle: "Indulge in premium travel and accommodation",
-    alt: "Ocean sunset view",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=1920&q=80",
-    title: "Natural Wonders",
-    subtitle: "Witness the beauty of untouched landscapes",
-    alt: "Ocean sunset view",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=1920&q=80",
-    title: "Natural Wonders",
-    subtitle: "Witness the beauty of untouched landscapes",
-    alt: "Ocean sunset view",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=1920&q=80",
-    title: "Natural Wonders",
-    subtitle: "Witness the beauty of untouched landscapes",
-    alt: "Ocean sunset view",
-  },
-];
 
 const heroSlides1: HeroSlide[] = [
   {
@@ -55,8 +20,8 @@ const Category = () => {
     <div>
       <HeroSlider slides={heroSlides1} />
       <main className="px-8">
-        {/* <DividerLine title="features" /> */}
-        {/* <FeatureComponent /> */}
+        <DividerLine title="features" />
+        <FeatureComponent />
         <ThreeCardDisplay title="regular section" />
         <ThreeCardDisplay title="special reports" />
       </main>
