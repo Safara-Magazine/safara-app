19767b98efe6684df1798300ecabc0927f8ed1e8
Olamilekan Aremu <aremu0235@gmail.com>
2026-01-31 18:14:30 +0100

    fix: build fixes

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,43 +1,11 @@
-"use client";
+// app/store/page.tsx
+import { Suspense } from "react";
+import Store from "@/components/store-components/StorePage";
 
-import BestSellers from "@/components/store-components/bestsellers";
-import Hero from "@/components/store-components/hero";
-import HighlightsSection from "@/components/store-components/store-highlights";
-import DealsSection from "@/components/store-components/store-grid";
-import NewArrivals from "@/components/store-components/new-arrivals";
-import StoreNavigation from "@/components/layout/Header/StoreNavBar";
-
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Store() {
-
-  const searchParams = useSearchParams();
-    const section = searchParams.get("section");
-
-   useEffect(() => {
-    if (!section) return;
-
-    const el = document.getElementById(section);
-
-    if (el) {
-      setTimeout(() => {
-        el.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
-      }, 200); 
-    }
-  }, [section]);
-
+export default function StorePage() {
   return (
-    <section className="">
-      <StoreNavigation />
-      <Hero />
-      <HighlightsSection />
-      <BestSellers />
-      <DealsSection />
-      <NewArrivals />
-    </section>
+    <Suspense fallback={<div>Loading store…</div>}>
+      <Store />
+    </Suspense>
   );
 }
